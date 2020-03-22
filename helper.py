0445297578a6e5d188b1b82aab1d9e52f52bbfe1
martin <martin@konfuzzyus.de>
2020-03-22 19:25:37 +0100

    removed unused Functiones

--- helper.py
+++ helper.py
@@ -12,34 +12,6 @@ def formatDateTime(input):
     arr = input.split('/')
     return '20' + arr[2] + '-' + arr[0] + '-' + arr[1]
 
-def Writezero3D(date, country, region):
-    try:
-        if (region == ''):
-            datatbaseConnect.insertPercentage3D(date, 0, country)
-        else:
-            datatbaseConnect.insertPercentage3D(date, 0, country + ", " + region)
-    except:
-        pass
-
-def Writezero5D(date, country, region):
-    try:
-        if (region == ''):
-            datatbaseConnect.insertPercentage5D(date, 0, country)
-        else:
-            datatbaseConnect.insertPercentage5D(date, 0, country + ", " + region)
-    except:
-        pass
-
-def Writezero7D(date, country, region):
-    try:
-        if (region == ''):
-            datatbaseConnect.insertPercentage7D(date, 0, country)
-        else:
-            datatbaseConnect.insertPercentage7D(date, 0, country + ", " + region)
-    except:
-        pass
-
-
 #### DO NOT CHANGE ####
 if __name__ == "__main__":
     exit(1)
