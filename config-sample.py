d3bc9f288d39349199a76e30fa91fe39e84ee912
martin <martin@konfuzzyus.de>
2020-03-22 13:35:32 +0100

    added main

---
+++ config-sample.py
@@ -0,0 +1,11 @@
+#### Database ####
+db_host = "localhost"
+db_port = "3306"
+db_scheme = "bmbf_formular"
+db_prefix = "bmbf_"
+db_user = "root"
+db_password = ""
+
+#### DO NOT CHANGE ####
+if __name__ == "__main__":
+    exit(1)
