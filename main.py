417a86acdc909cf055145fb2597e2fd416919f88
martin <martin@konfuzzyus.de>
2022-07-20 18:55:14 +0200

    improvements

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import sys
-
 import mysql.connector
 import config
-from pprint import pprint
-import datetime
-import helper
 import datatbaseConnect
 import csv
 import time
@@ -24,19 +20,21 @@ def main():
         spamreader = csv.reader(csvfile)
         totalCases = list(spamreader)
         l_main = len(totalCases)
-        #clear tables
         print('truncate')
+        truncateStart = time.time()
         datatbaseConnect.QueryDB(f"TRUNCATE {config.db_prefix}diff1d ;", False, dbConnection)
         datatbaseConnect.QueryDB(f"TRUNCATE {config.db_prefix}percentage1d ;", False, dbConnection)
         datatbaseConnect.QueryDB(f"TRUNCATE {config.db_prefix}percentage3d ;", False, dbConnection)
         datatbaseConnect.QueryDB(f"TRUNCATE {config.db_prefix}percentage5d ;", False, dbConnection)
         datatbaseConnect.QueryDB(f"TRUNCATE {config.db_prefix}percentage7d ;", False, dbConnection)
         datatbaseConnect.QueryDB(f"TRUNCATE {config.db_prefix}originalData ;", False, dbConnection)
-        print('truncate done')
+        truncateStop = time.time()
+        print(f'truncate done, took {truncateStop - truncateStart} seconds')
         # percentage for one day
         dataP1 = []
         dataD1 = []
         print("percentage for one day")
+        time1d = time.time()
         for i in range(1, l_main):
             len_side = len(totalCases[i])
             for j in range(4, len_side-1):
@@ -61,11 +59,17 @@ def main():
                         dataD1.append([totalCases[0][j + 1], diff, totalCases[i][1] + ", " + totalCases[i][0]])
                 except:
                     pass
+        time1dE = time.time()
+        print(f"Calculation of difference and percentage 1 day took {time1dE - time1d} seconds for {len(dataP1)} datapoints")
         datatbaseConnect.insertPercentage1D(dataP1, dbConnection)
+        time1dD1 = time.time()
         datatbaseConnect.insertDiffCases1D(dataD1, dbConnection)
+        time1dD = time.time()
+        print(f"Database took {time1dD1 - time1dE} seconds for inserting percentage 1 day and {time1dD - time1dD1} for inserting difference one day")
         # precentage for 3 days
         dataP3 = []
         print("percentage for 3 days")
+        time3d = time.time()
         for i in range(1, l_main):
             len_side = len(totalCases[i])
             try:
@@ -99,9 +103,14 @@ def main():
                         pass
             except:
                 continue
+        time3dE = time.time()
+        print(f"Calculation of percentage 3 days took {time3dE - time3d} seconds for {len(dataP3)} datapoints")
         datatbaseConnect.insertPercentage3D(dataP3, dbConnection)
+        time3dD = time.time()
+        print(f"Database took {time3dD - time3dE} seconds for inserting percentage 3 days")
         # precentage for 5 days
         dataP5 = []
+        time5d = time.time()
         print("percentage for 5 days")
         for i in range(1, l_main):
             len_side = len(totalCases[i])
@@ -155,10 +164,15 @@ def main():
                         pass
             except:
                 continue
+        time5dE = time.time()
+        print(f"Calculation of percentage 5 days took {time5dE - time5d} seconds for {len(dataP5)} datapoints")
         datatbaseConnect.insertPercentage5D(dataP5, dbConnection)
+        time5dD = time.time()
+        print(f"Database took {time5dD - time5dE} seconds for inserting percentage 5 days")
         # precentage for 7 days
         dataP7 = []
         print("percentage for 5 days")
+        time7d = time.time()
         for i in range(1, l_main):
             len_side = len(totalCases[i])
             try:
@@ -228,10 +242,15 @@ def main():
                         pass
             except:
                 continue
+        time7dE = time.time()
+        print(f"Calculation of percentage 5 days took {time7dE - time7d} seconds for {len(dataP7)} datapoints")
         datatbaseConnect.insertPercentage7D(dataP7, dbConnection)
+        time7dD = time.time()
+        print(f"Database took {time7dD - time7dE} seconds for inserting percentage 7 days")
         # total data
         dataT = []
         print("complete data")
+        timeT = time.time()
         for i in range(1, l_main):
             len_side = len(totalCases[i])
             for j in range(4, len_side):
@@ -254,7 +273,11 @@ def main():
                             dataT.append([totalCases[0][j], totalCases[i][j-1], totalCases[i][1] + ", " + totalCases[i][0]])
                 except:
                     pass
+        timeTE = time.time()
+        print(f"Collection of complete data took {timeTE - timeT} seconds for {len(dataT)} datapoints")
         datatbaseConnect.insertTotalCases(dataT, dbConnection)
+        timeTD = time.time()
+        print(f"Database took {timeTD - timeTE} seconds for inserting complete data")
     dbConnectionRaw.commit();
     dbConnectionRaw.close()
 
